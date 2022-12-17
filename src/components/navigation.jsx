f0bf12f14eec1739357dc17a8afcb794a9a4c32a
juniodeveloper <junio@juniodev.com>
2022-12-17 18:41:24 -0300

    deploy vercel

---
+++ src/components/navigation.jsx
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+import { useEffect } from 'react';
+import styles from '../styles/Navigation.module.css';
+
+const Navigation = () => {
+
+  useEffect(() => {
+    document.onscroll = function () { onScroll() };
+  })
+
+  function onScroll() {
+
+    const header = document.getElementById(styles.nav)
+    if (document.documentElement.scrollTop > 20 || document.body.scrollTop > 20) {
+      header.classList.add(styles.elevation)
+    } else {
+      header.classList.remove(styles.elevation)
+    }
+  }
+
+
+
+  return (
+    <div id={styles.nav}>
+      <div>
+        <Image
+          src="/logo_nav.svg"
+          alt="Logo"
+          width={65}
+          height={65}
+        />
+        <h1>TeleShare</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Navigation;
